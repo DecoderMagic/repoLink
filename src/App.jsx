1a4efc98e8bddc65c6029d5f3c99ee8724e9e57b
Researcher6898 <127759178+DecoderMagic@users.noreply.github.com>
2024-02-03 04:05:20 +0000

    Changes to be committed:     modified:   src/App.jsx     modified:   src/Components/About.jsx     modified:   src/Components/Home.jsx     modified:   src/Components/Portfolio.jsx     new file:   src/images/RobertGreeneBook.jpg     new file:   src/images/aboutMe.jpg     new file:   src/images/desk.jpg

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Luis Morrissey",
+  title: "Front End Developer",
+  email: "morrisseyluis@gmail.com",
+  gitHub: "DecoderMagic",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "Researcher6898",
 };
 
 const primaryColor = "#4E567E";
